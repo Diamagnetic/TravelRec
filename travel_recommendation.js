0981e6ca65df2a10bf4731ef62f59ad58948cec5
Chirag <c.d.marshall1869@gmail.com>
2024-06-16 12:13:36 -0400

    completed logic for string match

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,16 +1,30 @@
 function searchKeyword() {
     const navbarInput = document.getElementById('navbarSearch').trim().toLowerCase();
     const resultDiv = document.getElementById('searchResults');
-
-    let stringToCall = '';
     
-    if(/beach(es).*/.exec(navbarInput)) {
+    fetch('./travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        let stringToCall = '';
+        let resultData = [];
 
-    } else if(/countr(?:y|ies).*/.exec(navbarInput)) {
+        if(/beach(es).*/.exec(navbarInput)) {
+            stringToCall = 'beaches';
+        } else if(/countr(?:y|ies).*/.exec(navbarInput)) {
+            stringToCall = 'countries';
+    
+            data.countries.forEach((country) => resultData.push(...country.cities));
+        } else if(/temple(s).*/.exec(navbarInput)) {
+            stringToCall = 'temples';
+        } else {
+            resultDiv.innerHTML = `<div class = 'errorBox'>No result found for ${stringToCall}</div>`;
+            return ;
+        }
 
-    } else if(/temple(s).*/.exec(navbarInput)) {
+        if(stringToCall !== 'countries') {
+            resultData.push(...data.stringToCall);
+        }
 
-    } else {
-        resultDiv.innerHTML = `<div class = 'errorBox'>No result found for ${stringToCall}</div>`;
-    }
+        
+    });
 }
