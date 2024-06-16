ff7a7acede226f67d6bb7c4182d63d28ad3f47dd
Chirag <c.d.marshall1869@gmail.com>
2024-06-16 11:52:06 -0400

    added logic for string match

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,16 @@
 function searchKeyword() {
-    console.log('search clicked');
+    const navbarInput = document.getElementById('navbarSearch').trim().toLowerCase();
+    const resultDiv = document.getElementById('searchResults');
+
+    let stringToCall = '';
+    
+    if(/beach(es).*/.exec(navbarInput)) {
+
+    } else if(/countr(?:y|ies).*/.exec(navbarInput)) {
+
+    } else if(/temple(s).*/.exec(navbarInput)) {
+
+    } else {
+        resultDiv.innerHTML = `<div class = 'errorBox'>No result found for ${stringToCall}</div>`;
+    }
 }
