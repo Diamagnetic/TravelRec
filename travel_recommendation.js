28f098215f512ef794e934456e2dd162a88b9b80
Chirag <c.d.marshall1869@gmail.com>
2024-06-16 13:24:29 -0400

    corrected regex, achieved results for country, modified css

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,40 +1,45 @@
 function searchKeyword() {
-    const navbarInput = document.getElementById('navbarSearch').trim().toLowerCase();
+    const navbarInput = document.getElementById('navbarSearch').querySelector('#search').value.trim().toLowerCase();
     const resultDiv = document.getElementById('searchResults');
     resultDiv.innerHTML = '';
     
-    fetch('./travel_recommendation_api.json')
+    fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
         let stringToCall = '';
         let resultData = [];
 
-        if(/beach(es).*/.exec(navbarInput)) {
+        if(navbarInput.match(/beach(es)?.*/)) {
             stringToCall = 'beaches';
         } else if(/countr(?:y|ies).*/.exec(navbarInput)) {
             stringToCall = 'countries';
     
             data.countries.forEach((country) => resultData.push(...country.cities));
-        } else if(/temple(s).*/.exec(navbarInput)) {
+        } else if(/temple(s)?.*/.exec(navbarInput)) {
             stringToCall = 'temples';
         } else {
             resultDiv.innerHTML = `<div class = 'errorBox'>No result found for ${stringToCall}</div>`;
-            return ;
+            throw new Error('No result found for ' + stringToCall);
         }
+
+        console.log(data);
+        console.log(stringToCall);
 
         if(stringToCall !== 'countries') {
             resultData.push(...data.stringToCall);
         }
 
         resultData.forEach((result, index) => {
-            resultDiv.push(
+            resultDiv.innerHTML += 
                 `<div class = 'searchResult' id = 'seachResult: ${index}'>
                     <img src = './images/${result.imageUrl}'>
-                    <h3>${result.city}</h3>
+                    <h3>${result.name}</h3>
                     <p>${result.description}</p>
                     <input type = 'button' value = 'Visit'/>
-                </div>`
-            );
+                </div>`;
         });
+    })
+    .catch((error) => {
+        console.log('Error: ' + error)
     });
 }
