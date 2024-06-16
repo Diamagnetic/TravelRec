4f0ee7758fbd3d933bc89ed476645cdf9e99f18b
Chirag <c.d.marshall1869@gmail.com>
2024-06-16 16:34:41 -0400

    modified about

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -41,34 +41,3 @@ function searchKeyword() {
 function resetSearch() {
     document.getElementById('searchResults').innerHTML = '';
 }
-
-function updateAbout() {
-    const teamMembers = [
-        {
-            name : 'John Doe',
-            title: 'CEO',
-            description: 'John is responsible for',
-        },
-        {
-            name : 'Celina Thomas',
-            title: 'Team Lead',
-            description: 'Celina Thomas is responsible for',
-        },
-        {
-            name : 'Mike Tyson',
-            title: 'Delivery Head',
-            description: 'Mike Tyson is responsible for',
-        }
-    ];
-
-    const team = document.getElementsByClassName('teamMembers');
-    team.innerHTML = '';
-
-    debugger;
-    teamMembers.forEach((member) => {
-        team.innerHTML += `<h3 class = 'team'>${member.name}</h3>
-            <p class = 'teamDescription'>${member.description}</p>
-            <button class = 'teamTitle' disabled = true>${member.title}</button>
-        `;
-    });
-}
